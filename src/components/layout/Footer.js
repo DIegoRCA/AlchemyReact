de5b3d0bf86067bec042185417628dfe207e81b2
DiegoRCA <diegocartelle@gmail.com>
2022-02-22 16:42:15 +1100

    fotter

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import '../../styles/components/layout/Footer.css';
 const Footer = (props)=>{
     return(
         <div className="footer"><p> Tel: <span>(02) 9550 1321</span> - E-mail: <a target="_blank" href="mailto:info@alchemyco.com.au?Subject=Please%20contact%20me">info@alchemyco.com.au</a></p></div>
