19ec93fe5a38d2b18d4e35f6b8b2489d1b0abc1b
Diego Cartelle <diegocartelle@gmail.com>
2022-02-23 07:23:03 +1100

    e

--- src/pages/ServicesPage.js
+++ src/pages/ServicesPage.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import '../styles/components/pages/ServicesPage.css';
+
 
 const ServicesPage = (props)=>{
     return (
@@ -10,9 +12,9 @@ const ServicesPage = (props)=>{
         
     <div className='container-services'>
 
-        <div className='foto-services'>
+        <div className='foto-services '>
             <img src="./images/services/consultation.jpg" alt="img01"/>
-            <div className="texto-services">
+            <div className="texto-services centrado">
                 <h3>CONSULTATION &amp; DESIGN</h3>
                 <p>We act as translators, turning your great ideas into realities.</p>
             </div>
@@ -20,7 +22,7 @@ const ServicesPage = (props)=>{
 
         <div className='foto-services'>
             <img src="./images/services/tailored.jpg" alt="img02"/>
-            <div className="texto-services">
+            <div className="texto-services centrado">
                 <h3>TAILORED SOLUTIONS</h3>
                 <p>Unique products stimulate the imagination; we make brands stand out.</p>
             </div>
@@ -28,7 +30,7 @@ const ServicesPage = (props)=>{
         
         <div className='foto-services'>
             <img src="./images/services/alchemy go.jpg" alt="img03"/>
-            <div className="texto-services">
+            <div className="texto-services centrado">
                 <h3>ALCHEMY GO</h3>
                 <p>Must have add-ons and accessories to deliver immediate solutions.</p>
             </div>
@@ -36,7 +38,7 @@ const ServicesPage = (props)=>{
 
         <div className='foto-services'>
             <img src="./images/services/printing.jpg" alt="img04"/>
-            <div className="texto-services">
+            <div className="texto-services centrado">
                 <h3>DIGITAL PRINTING</h3>
                 <p>High quality print media in a wide range of formats, turning ideas into realities.</p>
             </div>
@@ -44,7 +46,7 @@ const ServicesPage = (props)=>{
 
         <div className='foto-services'>
             <img src="./images/services/logistic.jpg" alt="img05"/>
-            <div className="texto-services">
+            <div className="texto-services centrado">
                 <h3>LOGISTIGS</h3>
                 <p>Professional event production, installation and management. We build anywhere, anytime.</p>
             </div>
