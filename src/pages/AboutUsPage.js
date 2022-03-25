d7c136a81b60c1abfa5697de64bede2b5d74beab
DiegoRCA <diegocartelle@gmail.com>
2022-03-25 13:27:34 +1100

    about carousel

--- src/pages/AboutUsPage.js
+++ src/pages/AboutUsPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../styles/components/pages/AboutUsPage.css';
-
+import { Carousel } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
 const AboutUsPage = (props)=>{
     return (
         <main className='holder'>
@@ -14,41 +15,132 @@ const AboutUsPage = (props)=>{
     <p>Our Team prides itself on delivering <span>CREATIVITY, QUALITY &amp; PARTNERSHIP!</span></p>
         <p>We pride ourselves on helping customers achieve outstanding projects because</p>
         <span>“WE GIVE A SHIT!</span>
-</section>
+</section>           
 
-<div class="team">
-    <div class="people">
-        <img src="images/about/ani2.png" alt="Anita"/>
-        <div class="text">
-            <h2>Anita Martin Torres</h2>
-            <h6>Production Designer</h6>
-            <p>Multi-talented &amp; innovative in creating design solutions that look amazing-despite all sorts of jobs to deal with.</p>
-        </div>
-    </div>
-    <div class="people">
-        <img src="images/about/BEC2.png" alt="Bec"/>
-        <div class="text">
-            <h2>Bec <br/>Ianna</h2>
-            <h6>PROJECT MANAGER</h6>
-            <p>THEY CONVERT A CONCEPT INTO A REALITY THAT CAN BE BUILT ON TIME &amp; ON BUDGET. WITH OVER 20 YEARS OF COMBINED INDUSTRY EXPERIENCE, THEY DELIVER-CONSISTENTLY!</p>
-        </div>
-    </div>
-    <div class="people">
-        <img src="images/about/CHARLY3.png" alt="Charly"/>
-        <div class="text">
-            <h2>Charly Mcdonald</h2>
-            <h6>LOGISTIC SUPERVISOR</h6>
-            <p>YEARS OF EXPERIENCE INSTALLING A RANGE OF DIFFERENT PROJECTS IN ALL TYPES OF LOCATIONS-WITH CONSISTENT CUSTOMER SATISFACTION AND QUALITY.</p>
-        </div>
-    </div>
-    <div class="people">
-        <img src="images/about/Diego2-1.png" alt="Diego"/>
-        <div class="text">
-            <h2>Diego <br/>Cartelle</h2>
-            <h6>GRAPHICS MANAGER</h6>
-            <p>Multi-talented &amp; innovative in creating design solutions that look amazing-despite all sorts of jobs to deal with.</p>
-        </div>
-    </div>
+<div className="team-carousel">
+                <Carousel className="carousel-dark slide" interval="1000">
+                    <Carousel.Item>
+                    <div className=" d-block w-60 ">
+                        <img
+                        className=" d-block w-50"
+                        src="images\about\ani2.png"
+                        alt="vivid 2"
+                        />
+                        <Carousel.Caption className='text-team  w-75'>
+                        <h3 >ANITA MARTIN TORRES</h3>
+                        <h4>Production Designer</h4>
+                        <p>Multi-talented &amp; innovative in creating design solutions that look amazing-despite all sorts of jobs to deal with.</p>
+                        </Carousel.Caption>
+                    </div>
+                    </Carousel.Item>
+
+                    <Carousel.Item>
+                    <div className=" d-block w-60 ">
+                        <img
+                        className=" d-block x w-50"
+                        src="images\about\BEC2.png"
+                        alt="vivid 2"
+                        />
+                        <Carousel.Caption className='text-team  w-75'>
+                        <h3 >Bec Ianna</h3>
+                        <h4>PROJECT MANAGER</h4>
+                        <p>THEY CONVERT A CONCEPT INTO A REALITY THAT CAN BE BUILT ON TIME & ON BUDGET. WITH OVER 20 YEARS OF COMBINED INDUSTRY EXPERIENCE, THEY DELIVER-CONSISTENTLY!</p>
+                        </Carousel.Caption>
+                    </div>
+                    </Carousel.Item>
+
+                    <Carousel.Item>
+                    <div className=" d-block w-60 ">
+                        <img
+                        className=" d-block w-50"
+                        src="images\about\CHARLY3.png"
+                        alt="vivid 2"
+                        />
+                        <Carousel.Caption className='text-team  w-75'>
+                        <h3 >Charly Mcdonald</h3>
+                        <h4>LOGISTIC SUPERVISOR</h4>
+                        <p>YEARS OF EXPERIENCE INSTALLING A RANGE OF DIFFERENT PROJECTS IN ALL TYPES OF LOCATIONS-WITH CONSISTENT CUSTOMER SATISFACTION AND QUALITY.</p>
+                        </Carousel.Caption>
+                    </div>
+                    </Carousel.Item>
+
+                    <Carousel.Item>
+                    <div className=" d-block w-60 ">
+                        <img
+                        className=" d-block x w-50"
+                        src="images\about\Diego2-1.png"
+                        alt="vivid 2"
+                        />
+                        <Carousel.Caption className='text-team  w-75'>
+                        <h3 >DIEGO CARTELLE</h3>
+                        <h4>GRAPHICS MANAGER</h4>
+                        <p>YEARS OF EXPERIENCE INSTALLING A RANGE OF DIFFERENT PROJECTS IN ALL TYPES OF LOCATIONS-WITH CONSISTENT CUSTOMER SATISFACTION AND QUALITY.</p>
+                        </Carousel.Caption>
+                    </div>
+                    </Carousel.Item>
+
+                    <Carousel.Item>
+                    <div className=" d-block w-60 ">
+                        <img
+                        className=" d-block w-50"
+                        src="images\about\jaime2.png"
+                        alt="vivid 2"
+                        />
+                        <Carousel.Caption className='text-team  w-75'>
+                        <h3 >Jaime Merchan</h3>
+                        <h4>PROJECT MANAGER</h4>
+                        <p>THEY CONVERT A CONCEPT INTO A REALITY THAT CAN BE BUILT ON TIME & ON BUDGET. WITH OVER 20 YEARS OF COMBINED INDUSTRY EXPERIENCE, THEY DELIVER-CONSISTENTLY!</p>
+                        </Carousel.Caption>
+                    </div>
+                    </Carousel.Item>
+
+                    <Carousel.Item>
+                    <div className=" d-block w-60 ">
+                        <img
+                        className=" d-block x w-50"
+                        src="images\about\JC1.png"
+                        alt="vivid 2"
+                        />
+                        <Carousel.Caption className='text-team  w-75'>
+                        <h3 >John Chellingworth</h3>
+                        <h4>CFO</h4>
+                        <p>FORMER CEO THAT ENSURES THAT CASH FLOW IS MANAGED &amp; SYSTEMS DELIVER PROFITS.</p>
+                        </Carousel.Caption>
+                    </div>
+                    </Carousel.Item>
+
+                    <Carousel.Item>
+                    <div className=" d-block w-60 ">
+                        <img
+                        className=" d-block w-50"
+                        src="images\about\MATT2.png"
+                        alt="vivid 2"
+                        />
+                        <Carousel.Caption className='text-team  w-75'>
+                        <h3 >Matt Aylife</h3>
+                        <h4>CREATIVE DIRECTOR</h4>
+                        <p>20 YEARS OF DEVELOPING HIGH QUALITY &amp; CREATIVE SOLUTIONS COMBINED WITH FAIR PRICING.</p>
+                        </Carousel.Caption>
+                    </div>
+                    </Carousel.Item>
+
+                    <Carousel.Item>
+                    <div className=" d-block w-60 ">
+                        <img
+                        className=" d-block x w-50"
+                        src="images\about\Tony.png"
+                        alt="vivid 2"
+                        />
+                        <Carousel.Caption className='text-team  w-75'>
+                        <h3 >Anthony Shone</h3>
+                        <h4>PRODUCTION MANAGER</h4>
+                        <p>EXTENSIVE BUILD &amp; MANUFACTURING EXPERIENCE WITH STATE-OF-THE-ART EQUIPMENT AND A PRODUCTION TEAM THAT EXCELS.</p>
+                        </Carousel.Caption>
+                    </div>
+                    </Carousel.Item>
+                </Carousel>
+</div>
+{/* 
     <div class="people">
         <img src="images/about/jaime2.png" alt="Jaime"/>
         <div class="text">
@@ -81,7 +173,7 @@ const AboutUsPage = (props)=>{
             <p>EXTENSIVE BUILD &amp; MANUFACTURING EXPERIENCE WITH STATE-OF-THE-ART EQUIPMENT AND A PRODUCTION TEAM THAT EXCELS</p>
         </div>
     </div>
-</div>
+</div> */}
 <div className='mision'>
                 <h1>OUR MISSION</h1>
                 <p><span>"You imagine it, we create it"</span> is our charter here at Alchemy. With over 20 years experience in custom set and display manufacturing, the company has worked with over 100 of Australia’s leading brand agencies, and with over 2000 installations achieved we know our stuff. Our team of local and European craftspeople are multi-talented in all trades to produce high quality creations to represent your brand or environment.</p>
--- src/pages/ServicesPage.js
+++ src/pages/ServicesPage.js
@@ -22,7 +22,31 @@ const ServicesPage = (props)=>{
             <div className="centrado1">
                 <h3>CONSULTATION &amp; DESIGN</h3>
                 <p>We act as translators, turning your great ideas into realities.</p>
-                <button className="button" onClick={function sayHello(){alert('At Alchemy we offer results by translating your ideas and designs through a creative and collaboration process, which allows our multi-talented team to be an extension of your production service.Through clever design, project management and production, our team approaches each project with innovative forward thinking to deliver outcomes for brands and agencies.We also incorporate a Hot-Desk environment for our project partners so they can engage directly with our community.Alchemy is Australian owned and operated; our consultation and design hub offers a complete end to end production facility under one roof, 15 minutes from the Sydney CBD.')}}>Read More</button>
+
+                <>
+                  {['left'].map((placement) => (
+                    <OverlayTrigger
+                      trigger="click"
+                      key={placement}
+                      placement={placement}
+                      overlay={
+                        <Popover id={`popover-positioned-${placement}`}>
+                          <Popover.Header as="h3">{<strong>CONSULTATION & DESIGN</strong>}</Popover.Header>
+                          <Popover.Body>
+                          At Alchemy we offer results by translating your ideas and designs through a creative and collaboration process, which allows our multi-talented team to be an extension of your production service.
+
+                          Through clever design, project management and production, our team approaches each project with innovative forward thinking to deliver outcomes for brands and agencies.
+                          We also incorporate a Hot-Desk environment for our project partners so they can engage directly with our community.
+
+                          Alchemy is Australian owned and operated; our consultation and design hub offers a complete end to end production facility under one roof, 15 minutes from the Sydney CBD.          </Popover.Body>
+                        </Popover>
+                      }
+                    >
+                      <Button className="button" variant="light">Read More</Button>
+                    </OverlayTrigger>
+                  ))}
+                </>            
+            
             </div>
         </div>
 
@@ -30,8 +54,26 @@ const ServicesPage = (props)=>{
         <div className="centrado2">
                 <h3>TAILORED SOLUTIONS</h3>
                 <p>Unique products stimulate the imagination; we make brands stand out.</p>
-                <button className="button" onClick={function sayHello(){alert('Alchemy’s creative works encompass everything from experiential projects, brand elements, film & TV, exhibitions, displays, stage sets, sculpture, and installations. We incorporate a “work art balance” allowing for creativity and quality through our community of craftsmen and women, all of whom are experts in creating semi-permanent brand solutions.This collective has been tasked with delivering for some of Australia’s most creative agencies and global brands with a full service 1500 sqm workshop and creative hub.')}}>Read More</button>
+                <>
+                  {['bottom'].map((placement) => (
+                    <OverlayTrigger
+                      trigger="click"
+                      key={placement}
+                      placement={placement}
+                      overlay={
+                        <Popover id={`popover-positioned-${placement}`}>
+                          <Popover.Header as="h3">{<strong>TAILORED SOLUTIONS</strong>}</Popover.Header>
+                          <Popover.Body>
+                          Alchemy’s creative works encompass everything from experiential projects, brand elements, film & TV, exhibitions, displays, stage sets, sculpture, and installations. We incorporate a “work art balance” allowing for creativity and quality through our community of craftsmen and women, all of whom are experts in creating semi-permanent brand solutions.
 
+                          This collective has been tasked with delivering for some of Australia’s most creative agencies and global brands with a full service 1500 sqm workshop and creative hub.</Popover.Body>
+                        </Popover>
+                      }
+                    >
+                      <Button className="button" variant="light">Read More</Button>
+                    </OverlayTrigger>
+                  ))}
+                </>  
             </div>
             <img src="./images/services/tailored.jpg" alt="img02"/>
            
@@ -42,8 +84,25 @@ const ServicesPage = (props)=>{
             <div className="centrado1">
                 <h3>ALCHEMY GO</h3>
                 <p>Must have add-ons and accessories to deliver immediate solutions.</p>
-                <button className="button" onClick={function sayHello(){alert('From backstage to front of house, Alchemy Go offers a range of event hire solutions available for turnkey projects. Whether it’s accessories for a bustling backstage environment, high impact branding for a trade show or pop-up activations, we have a selected range of accessories for these applications.Alchemy Go products include display plinths, trade show shell scheme wraps and skins, through to backstage make-up mirrors and catwalks. Want something more bespoke? Talk to our Tailored Solutions team to amp up your event or display requirements.')}}>Read More</button>
-            </div>
+                <>
+                  {['left'].map((placement) => (
+                    <OverlayTrigger
+                      trigger="click"
+                      key={placement}
+                      placement={placement}
+                      overlay={
+                        <Popover id={`popover-positioned-${placement}`}>
+                          <Popover.Header as="h3">{<strong>ALCHEMY GO</strong>}</Popover.Header>
+                          <Popover.Body>
+                          From backstage to front of house, Alchemy Go offers a range of event hire solutions available for turnkey projects. Whether it’s accessories for a bustling backstage environment, high impact branding for a trade show or pop-up activations, we have a selected range of accessories for these applications.
+                          Alchemy Go products include display plinths, trade show shell scheme wraps and skins, through to backstage make-up mirrors and catwalks. Want something more bespoke? Talk to our Tailored Solutions team to amp up your event or display requirements.</Popover.Body>
+                        </Popover>
+                      }
+                    >
+                      <Button className="button" variant="light">Read More</Button>
+                    </OverlayTrigger>
+                  ))}
+                </>             </div>
         </div>
 
         <div className='foto-services foto2'>
@@ -52,24 +111,24 @@ const ServicesPage = (props)=>{
                 <p>High quality print media in a wide range of formats, turning ideas into realities.</p>
 
                 <>
-  {['right'].map((placement) => (
-    <OverlayTrigger
-      trigger="click"
-      key={placement}
-      placement={placement}
-      overlay={
-        <Popover id={`popover-positioned-${placement}`}>
-          <Popover.Header as="h3">{<strong>DIGITAL PRINTING</strong>}</Popover.Header>
-          <Popover.Body>
-            From backstage to front of house, Alchemy Go offers a range of event hire solutions available for turnkey projects. Whether it’s accessories for a bustling backstage environment, high impact branding for a trade show or pop-up activations, we have a selected range of accessories for these applications.Alchemy Go products include display plinths, trade show shell scheme wraps and skins, through to backstage make-up mirrors and catwalks. Want something more bespoke? Talk to our Tailored Solutions team to amp up your event or display requirements.
-          </Popover.Body>
-        </Popover>
-      }
-    >
-      <Button className="button" variant="light">Read More</Button>
-    </OverlayTrigger>
-  ))}
-</>
+                  {['bottom'].map((placement) => (
+                    <OverlayTrigger
+                      trigger="click"
+                      key={placement}
+                      placement={placement}
+                      overlay={
+                        <Popover id={`popover-positioned-${placement}`}>
+                          <Popover.Header as="h3">{<strong>DIGITAL PRINTING</strong>}</Popover.Header>
+                          <Popover.Body>
+                            From backstage to front of house, Alchemy Go offers a range of event hire solutions available for turnkey projects. Whether it’s accessories for a bustling backstage environment, high impact branding for a trade show or pop-up activations, we have a selected range of accessories for these applications.Alchemy Go products include display plinths, trade show shell scheme wraps and skins, through to backstage make-up mirrors and catwalks. Want something more bespoke? Talk to our Tailored Solutions team to amp up your event or display requirements.
+                          </Popover.Body>
+                        </Popover>
+                      }
+                    >
+                      <Button className="button" variant="light">Read More</Button>
+                    </OverlayTrigger>
+                  ))}
+                </>
 
 
             </div>
@@ -82,8 +141,25 @@ const ServicesPage = (props)=>{
             <div className="centrado1">
                 <h3>LOGISTIGS</h3>
                 <p>Professional event production, installation and management. We build anywhere, anytime.</p>
-                <button className="button" onClick={function sayHello(){alert('From backstage to front of house, Alchemy Go offers a range of event hire solutions available for turnkey projects. Whether it’s accessories for a bustling backstage environment, high impact branding for a trade show or pop-up activations, we have a selected range of accessories for these applications.Alchemy Go products include display plinths, trade show shell scheme wraps and skins, through to backstage make-up mirrors and catwalks. Want something more bespoke? Talk to our Tailored Solutions team to amp up your event or display requirements.')}}>Read More</button>
-
+                <>
+                  {['left'].map((placement) => (
+                    <OverlayTrigger
+                      trigger="click"
+                      key={placement}
+                      placement={placement}
+                      overlay={
+                        <Popover id={`popover-positioned-${placement}`}>
+                          <Popover.Header as="h3">{<strong>LOGISTIGS</strong>}</Popover.Header>
+                          <Popover.Body>
+                            From backstage to front of house, Alchemy Go offers a range of event hire solutions available for turnkey projects. Whether it’s accessories for a bustling backstage environment, high impact branding for a trade show or pop-up activations, we have a selected range of accessories for these applications.Alchemy Go products include display plinths, trade show shell scheme wraps and skins, through to backstage make-up mirrors and catwalks. Want something more bespoke? Talk to our Tailored Solutions team to amp up your event or display requirements.
+                          </Popover.Body>
+                        </Popover>
+                      }
+                    >
+                      <Button className="button" variant="light">Read More</Button>
+                    </OverlayTrigger>
+                  ))}
+                </>
             </div>
         </div>
 
