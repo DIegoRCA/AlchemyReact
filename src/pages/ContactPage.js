2a12f9bec06762d554d800083326e7ac00ed2998
DiegoRCA <diegocartelle@gmail.com>
2022-05-16 12:26:48 +1000

    post

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -28,7 +28,7 @@ const ContactPage = (props)=>{
             e.preventDefault();
             setMsg('');
             setSending(true)
-            const response = await axios.post(`&{process.env.REACT_APP_API_URL}/api/contacto`, formData);
+            const response = await axios.post(`${process.env.REACT_APP_API_URL}/api/contacto`, formData);
             setSending(false);
             setMsg(response.data.message);
             if (response.data.error === false){
